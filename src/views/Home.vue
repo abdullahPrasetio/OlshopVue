ea59d4e4950d1aa72275e58760996171b6a3c5b2
AbdullahPrasetio <adeprastio1996@gmail.com>
2020-08-01 23:16:57 +0700

    Belajar Commit

---
+++ src/views/Home.vue
@@ -0,0 +1,34 @@
+<template>
+  <div class="home">
+    <HeaderOlshop/>
+    <HeroOlshop/>
+    <WomenOlshop/>
+    <InstaOlshop/>
+    <LogoOlshop/>
+    <FooterOlshop/>
+
+  </div>
+</template>
+
+<script>
+// // @ is an alias to /src
+// import HelloWorld from '@/components/HelloWorld.vue'
+import HeaderOlshop from '@/components/HeaderOlshop.vue';
+import HeroOlshop from '@/components/HeroOlshop.vue';
+import WomenOlshop from '@/components/WomenOlshop.vue';
+import InstaOlshop from '@/components/InstaOlshop.vue';
+import LogoOlshop from '@/components/LogoOlshop.vue';
+import FooterOlshop from '@/components/FooterOlshop.vue';
+
+export default {
+  name: 'Home',
+  components: {
+    HeaderOlshop,
+    HeroOlshop,
+    WomenOlshop,
+    InstaOlshop,
+    LogoOlshop,
+    FooterOlshop
+  }
+}
+</script>
